7984e08f9611725bddd4abf1a4db4b8f8954ad54
Salman Moazam <salmanmoazam08@gmail.com>
2021-04-17 12:51:33 +0500

    changed node.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,25 @@
-// /**
-//  * Configure your Gatsby site with this file.
-//  *
-//  * See: https://www.gatsbyjs.com/docs/gatsby-config/
-//  * url:
-//           "https://virtual-lolly-by-mateen.netlify.app/.netlify/functions/lollies",
-//  */
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ * url:
+        "https://virtual-lolly-by-mateen.netlify.app/.netlify/functions/lollies",
+ */
 
-// module.exports = {
-//   /* Your site config here */
-//   plugins: [
-//     {
-//       resolve: "gatsby-source-graphql",
-//       options: {
-//         // This type will contain remote schema Query type
-//         typeName: "GCMS",
-//         // This is field under which it's accessible
-//         fieldName: "get_lollies",
-//         // Url to query from
-        
-//       },
-//     },
-//   ],
-// }
+module.exports = {
+  /* Your site config here */
+plugins: [
+    {
+    resolve: "gatsby-source-graphql",
+    options: {
+        // This type will contain remote schema Query type
+        typeName: "Lollies",
+        // This is field under which it's accessible
+        fieldName: "get_lollies",
+        // Url to query from
+        url: "https://607a9209b62f2000079f50c9--serene-mahavira-bafc5f.netlify.app/.netlify/functions/vCard"
+    },
+    },
+],
+}
+// LOLLIES
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,61 +1,91 @@
-// // const path = require("path")
-
-// // exports.createPages = async ({ graphql, actions }) => {
-// //     const { createPage } = actions
-// //     const response = await graphql(`
-// //         query {
-// //             allGraphCmsBlog {
-// //                 nodes {
-// //                     link
-// //                 }
-// //             }
-// //         }
-// //     `)
-
-// // response.data.allGraphCmsBlog.nodes.forEach(node => {
-// //     createPage({
-// //         path: `/showLolly/${node.link}`,
-// //         component: path.resolve("./src/templates/sendingTemp.js"),
-// //         context: {
-// //             link: node.link,
-// //         },
-// //     })
-// //     })
-// // }
-
 // const path = require("path")
-// exports.createPages = async ({ graphql, actions }) => {
-// const { createPage } = actions
 
-// const { data } = await graphql(`
-//     {
-//         get_lollies {
-//             getVCard {
-//             c1
-//             c2
-//             c3
-//             link
-//             sender
-//             rec
-//             msg
+// exports.createPages = async ({ graphql, actions }) => {
+//     const { createPage } = actions
+//     const response = await graphql(`
+//         query {
+//             allGraphCmsBlog {
+//                 nodes {
+//                     link
+//                 }
 //             }
 //         }
-//     }
-// `)
+//     `)
 
-// data.get_lollies.getVCard.forEach(node => {
+// response.data.allGraphCmsBlog.nodes.forEach(node => {
 //     createPage({
-//         path: `showLolly/${node.link}`,
+//         path: `/showLolly/${node.link}`,
 //         component: path.resolve("./src/templates/sendingTemp.js"),
 //         context: {
-//             c1: node.c1,
-//             c2: node.c2,
-//             c3: node.c3,
 //             link: node.link,
-//             msg: node.msg,
-//             sender: node.sender,
-//             rec: node.rec,
 //         },
 //     })
-// })
+//     })
 // }
+
+const path = require("path")
+exports.createPages = async ({ graphql, actions }) => {
+const { createPage } = actions
+
+const { data } = await graphql(`
+    {
+        get_lollies {
+            getVCard {
+            c1
+            c2
+            c3
+            link
+            sender
+            rec
+            msg
+            }
+        }
+    }
+`)
+
+data.get_lollies.getVCard.forEach(node => {
+    createPage({
+        path: `showLolly/${node.link}`,
+        component: path.resolve("./src/templates/sendingTemp.js"),
+        context: {
+            c1: node.c1,
+            c2: node.c2,
+            c3: node.c3,
+            link: node.link,
+            msg: node.msg,
+            sender: node.sender,
+            rec: node.rec,
+        },
+    })
+})
+}
+
+
+
+// const result = await graphql(`
+// query MyQuery {
+//   LOLLIES {
+//     getLollies {
+//       id
+//       colorTop
+//       colorMiddle
+//       colorBottom
+//       recipient
+//       message
+//       sender
+//       lollyPath
+//     }
+//   }
+// }
+// `);
+
+// console.log(result);
+// result.data.LOLLIES.getLollies.map((data) => {
+// createPage({
+//   path: `${data.lollyPath}`,
+//   component: path.resolve("./src/Template/Template.tsx"),
+//   context: {
+//     data: data,
+//   },
+// });
+// });
